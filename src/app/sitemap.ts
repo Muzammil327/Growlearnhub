6aa219b37926a6bf36757ab7690ba780af9bf09c
Muzammil327 <mmuzammiloff327@gmail.com>
2024-05-24 09:39:28 +0500

    updated

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -4,71 +4,71 @@ const URL = process.env.NEXT_PUBLIC_BACKEND_URL;
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   try {
-    // response 1
-    const response1 = await fetch(`${URL}/api/get/allmcqs`, {
-      cache: "no-store", // This will bypass cache
-    });
-    const data1 = await response1.json();
+    // // response 1
+    // const response1 = await fetch(`${URL}/api/get/allmcqs`, {
+    //   cache: "no-store", // This will bypass cache
+    // });
+    // const data1 = await response1.json();
 
-    const mcqsroutes: MetadataRoute.Sitemap = data1.map((product: any) => ({
-      url: `${
-        process.env.NEXT_PUBLIC_FRONTEND_URL
-      }/mcqs/${convertToLowercaseWithHyphen(product.title)}/`,
-      lastModified: new Date().toISOString(),
-      priority: 0.6,
-      changeFrequency: "daily",
-    }));
-    // response 2
+    // const mcqsroutes: MetadataRoute.Sitemap = data1.map((product: any) => ({
+    //   url: `${
+    //     process.env.NEXT_PUBLIC_FRONTEND_URL
+    //   }/mcqs/${convertToLowercaseWithHyphen(product.title)}/`,
+    //   lastModified: new Date().toISOString(),
+    //   priority: 0.6,
+    //   changeFrequency: "daily",
+    // }));
+    // // response 2
 
-    const response2 = await fetch(`${URL}/api/get/book`, {
-      cache: "no-store", // This will bypass cache
-    });
-    const data2 = await response2.json();
+    // const response2 = await fetch(`${URL}/api/get/book`, {
+    //   cache: "no-store", // This will bypass cache
+    // });
+    // const data2 = await response2.json();
 
-    const bookroutes: MetadataRoute.Sitemap = data2.map((product: any) => ({
-      url: `${
-        process.env.NEXT_PUBLIC_FRONTEND_URL
-      }/quiz/${convertToLowercaseWithHyphen(product.title)}/`,
-      lastModified: new Date().toISOString(),
-      priority: 0.9,
-      changeFrequency: "daily",
-    }));
-    // response 3
+    // const bookroutes: MetadataRoute.Sitemap = data2.map((product: any) => ({
+    //   url: `${
+    //     process.env.NEXT_PUBLIC_FRONTEND_URL
+    //   }/quiz/${convertToLowercaseWithHyphen(product.title)}/`,
+    //   lastModified: new Date().toISOString(),
+    //   priority: 0.9,
+    //   changeFrequency: "daily",
+    // }));
+    // // response 3
 
-    const response3 = await fetch(`${URL}/api/get/heading1`, {
-      cache: "no-store", // This will bypass cache
-    });
-    const data3 = await response3.json();
+    // const response3 = await fetch(`${URL}/api/get/heading1`, {
+    //   cache: "no-store", // This will bypass cache
+    // });
+    // const data3 = await response3.json();
 
-    const heading1routes: MetadataRoute.Sitemap = data3.map((product: any) => ({
-      url: `${
-        process.env.NEXT_PUBLIC_FRONTEND_URL
-      }/quiz/${convertToLowercaseWithHyphen(
-        product.book.title
-      )}/${convertToLowercaseWithHyphen(product.title)}/`,
-      lastModified: new Date().toISOString(),
-      priority: 0.9,
-      changeFrequency: "daily",
-    }));
-    // response 4
+    // const heading1routes: MetadataRoute.Sitemap = data3.map((product: any) => ({
+    //   url: `${
+    //     process.env.NEXT_PUBLIC_FRONTEND_URL
+    //   }/quiz/${convertToLowercaseWithHyphen(
+    //     product.book.title
+    //   )}/${convertToLowercaseWithHyphen(product.title)}/`,
+    //   lastModified: new Date().toISOString(),
+    //   priority: 0.9,
+    //   changeFrequency: "daily",
+    // }));
+    // // response 4
 
-    const response4 = await fetch(`${URL}/api/get/heading2`, {
-      cache: "no-store", // This will bypass cache
-    });
-    const data4 = await response4.json();
+    // const response4 = await fetch(`${URL}/api/get/heading2`, {
+    //   cache: "no-store", // This will bypass cache
+    // });
+    // const data4 = await response4.json();
 
-    const heading2routes: MetadataRoute.Sitemap = data4.map((product: any) => ({
-      url: `${
-        process.env.NEXT_PUBLIC_FRONTEND_URL
-      }/quiz/${convertToLowercaseWithHyphen(
-        product.book.title
-      )}/${convertToLowercaseWithHyphen(
-        product.heading1.title
-      )}/${convertToLowercaseWithHyphen(product.title)}/`,
-      lastModified: new Date().toISOString(),
-      priority: 0.8,
-      changeFrequency: "daily",
-    }));
+    // const heading2routes: MetadataRoute.Sitemap = data4.map((product: any) => ({
+    //   url: `${
+    //     process.env.NEXT_PUBLIC_FRONTEND_URL
+    //   }/quiz/${convertToLowercaseWithHyphen(
+    //     product.book.title
+    //   )}/${convertToLowercaseWithHyphen(
+    //     product.heading1.title
+    //   )}/${convertToLowercaseWithHyphen(product.title)}/`,
+    //   lastModified: new Date().toISOString(),
+    //   priority: 0.8,
+    //   changeFrequency: "daily",
+    // }));
 
     return [
       {
@@ -197,10 +197,10 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
         priority: 1,
         changeFrequency: "daily",
       },
-      ...bookroutes,
-      ...heading1routes,
-      ...heading2routes,
-      ...mcqsroutes,
+      // ...bookroutes,
+      // ...heading1routes,
+      // ...heading2routes,
+      // ...mcqsroutes,
     ];
   } catch (error) {
     console.error("Error", error);
