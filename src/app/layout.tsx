ddbf522a6bb81a5e26ae4ea77781626925353897
Muzammil327 <mmuzammiloff327@gmail.com>
2024-06-07 12:12:19 +0500

    updated class 9/10 book

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,58 +32,43 @@ export default function RootLayout({
   return (
     <html lang="en" className="scroll-smooth dark">
       <head>
-        <meta name="application-name" content={data.applicationName} />
-        <meta name="author" content={data.authorName} />
-        <link rel="author" href="https://growlearnhub.com/" />
-        <meta name="generator" content={data.applicationName} />
-        <meta name="referrer" content="origin-when-cross-origin" />
-        <meta name="color-scheme" content="light" />
-        <meta name="creator" content={data.domain} />
-        <meta name="publisher" content={data.domain} />
-        {/* og */}
-        <meta property="og:type" content="website" />
-        <meta property="og:site_name" content={data.domain} />
-        <meta property="og:locale" content="en_US" />
-        <meta property="og:image:width" content="1200" />
-        <meta property="og:image:height" content="630" />
-        {/* icon */}
-        <meta name="theme-color" content="#f73e3e" />
-        <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
-        <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+    {/* -- Metadata -- */}
+    <meta name="application-name" content={data.applicationName} />
+    <meta name="author" content={data.authorName} />
+    <meta name="generator" content={data.applicationName} />
+    <meta name="referrer" content="origin-when-cross-origin" />
+    <meta name="color-scheme" content="light" />
+    <meta name="creator" content={data.domain} />
+    <meta name="publisher" content={data.domain} />
+    
+    {/* -- OG (Open Graph) -- */}
+    <meta property="og:type" content="website" />
+    <meta property="og:site_name" content={data.domain} />
+    <meta property="og:locale" content="en_US" />
+    <meta property="og:image:width" content="1200" />
+    <meta property="og:image:height" content="630" />
+    
+    {/* -- Theme and Icons -- */}
+    <meta name="theme-color" content="#f73e3e" />
+    <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+    <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+    <link rel="apple-touch-icon" href="/apple-touch-icon.png" type="image/png" />
+    <link rel="apple-touch-icon" href="/favicon-16x16.png" sizes="16x16" type="image/png" />
+    <link rel="apple-touch-icon" href="/favicon-32x32.png" sizes="32x32" type="image/png" />
+    
+    {/* -- Twitter -- */}
+    <meta name="twitter:card" content="summary" />
+    <meta name="twitter:creator" content="@growlearnhub" />
+    <meta property="twitter:domain" content={data.domain} />
+    
+    {/* -- Verification -- */}
+    <meta name="ahrefs-site-verification" content="234939215ab82c4e0069a4dfb6f90dcda6f5777af678e3dffac7654efdddbf42" />
+    <meta name="google-site-verification" content="1VFdWs-EKGhKENv4V6Weuup3qg0kkJGDwoI2gzRryEI" />
+    
+    {/* -- Preconnect -- */}
+    <link rel="preconnect" href="https://www.googletagmanager.com" />
+</head>
 
-        <link
-          rel="apple-touch-icon"
-          href="/apple-touch-icon.png"
-          type="image/png"
-        />
-
-        <link
-          rel="apple-touch-icon"
-          href="/favicon-16x16.png"
-          sizes="16x16"
-          type="image/png"
-        />
-        <link
-          rel="apple-touch-icon"
-          href="/favicon-32x32.png"
-          sizes="32x32"
-          type="image/png"
-        />
-        {/* twitter */}
-        <meta name="twitter:card" content="summary" />
-        <meta name="twitter:creator" content="@growlearnhub" />
-        <meta property="twitter:domain" content={data.domain} />
-        {/* ahref  */}
-        <meta
-          name="ahrefs-site-verification"
-          content="234939215ab82c4e0069a4dfb6f90dcda6f5777af678e3dffac7654efdddbf42"
-        />
-        {/* google search console  */}
-        <meta
-          name="google-site-verification"
-          content="1VFdWs-EKGhKENv4V6Weuup3qg0kkJGDwoI2gzRryEI"
-        />
-      </head>
       <body className={inter.className}>
         <ThemeSwitcher>{children}</ThemeSwitcher>
       </body>
