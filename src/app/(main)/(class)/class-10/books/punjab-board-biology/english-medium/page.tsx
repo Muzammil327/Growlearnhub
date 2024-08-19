cfaa38d46472ee7f5b367f82f2a74ae7b5c0039e
Muzammil327 <mmuzammiloff327@gmail.com>
2024-08-19 08:16:14 +0500

    updated: 8:07 8/19/2024

--- src/app/(main)/(class)/class-10/books/punjab-board-biology/english-medium/page.tsx
+++ src/app/(main)/(class)/class-10/books/punjab-board-biology/english-medium/page.tsx
@@ -1,7 +1,199 @@
 import React from 'react'
+import { Metadata } from 'next'
+import Link from 'next/link'
+import Wrapper from '@/src/components/element/Wrapper'
+import PDFViewer from '@/src/components/element/PDFViewer'
+import CardSmall from '@/src/components/card/cardSmall/cardSmall'
+import { MainDataClass10Book } from '@/src/app/(main)/(class)/class-10/books/data'
 
-export default function page() {
+const data = {
+  title:
+    '10th Class Biology Book PDF English Medium Free Download | Punjab Boards',
+  description:
+    'Download the 10th Class Biology Book PDF in English Medium for free. Get the Punjab Board-approved textbook and explore chapter-wise downloads, along with other Class 10 books in both English and Urdu mediums.',
+  canonical: '/class-10/books/punjab-board-biology/english-medium/',
+  index: true,
+  follow: true,
+  image: '/books/class-10/punjab/class-10-biology-book-punjab-board.webp',
+  url: 'https://growlearnhub.com/class-10/books/punjab-board-biology/english-medium/',
+  keywords: [
+    'biology class 10 punjab text book pdf',
+    'class 10 biology book pdf punjab board',
+    '10th class biology punjab text book',
+    'biology book class 10 punjab board',
+    '10th Class Biology Book PDF English Medium Free Download',
+    'class 10 biology book punjab board',
+    'punjab text book biology 10th class pdf english medium',
+  ],
+  fileId_english: '1MFRZ5yO0SrrfTkAM_ewXBp_Pec4zTOA-',
+  clist: [
+    {
+      id: 0,
+      name: 'Class 9 Books',
+      slug: '/class-9/books/',
+    },
+    {
+      id: 1,
+      name: 'Class 11 Books',
+      slug: '/class-11/books/',
+    },
+    {
+      id: 2,
+      name: 'Class 12 Books',
+      slug: '/class-12/books/',
+    },
+    {
+      id: 3,
+      name: 'VU cgpa Calculator',
+      slug: '/vu/mid-mark-calculator/',
+    },
+    {
+      id: 4,
+      name: 'Class 9 Mcqs',
+      slug: '/class-9/mcqs/',
+    },
+    {
+      id: 5,
+      name: 'Class 11 Mcqs',
+      slug: '/class-11/mcqs/',
+    },
+    {
+      id: 6,
+      name: 'Class 12 Mcqs',
+      slug: '/class-12/mcqs/',
+    },
+  ],
+}
+
+export default function Page() {
   return (
-    <div>page</div>
+    <Wrapper
+      title={data.title}
+      url={data.canonical}
+      b1="10th Class"
+      b1Link="/class-10/"
+      b2="Books"
+      b2Link="/class-10/books/"
+      b3="Punjab Board Biology"
+      b3Link="/class-10/books/punjab-board-biology/"
+      b4="Biology 10th Class Book English Medium"
+      image={data.image}
+      clist={data.clist}
+    >
+      <h3>Biology 10th Class Book English Medium PDF Download Punjab Board</h3>
+      <p>
+        Are you looking for a <strong>Class 10 biology book punjab board</strong>
+        ? Here, you can access all the books of class 10 you need for free. Our
+        website offers a complete collection of books for students in Class 9,
+        10, 11, and 12. Whether you&apos;re preparing for exams or need extra
+        resources, we&apos;ve got you covered.
+      </p>
+      <table className="relative w-full">
+        <thead>
+          <tr className="bg-white border-b hover:bg-slate-100">
+            <th
+              scope="row"
+              className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap"
+            >
+              Chapter Wise Download Biology Book
+            </th>
+            <td className="px-6 py-4"></td>
+          </tr>
+        </thead>
+        <tbody>
+          {chapterDATA.map((data: any) => {
+            return (
+              <tr
+                className="bg-white border-b hover:bg-slate-100"
+                key={data.id}
+              >
+                <th
+                  scope="row"
+                  className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap"
+                >
+                  Chapter {data.id} Biology Book
+                </th>
+                <td className="px-6 py-4">
+                  <Link href={data.link}>Download PDF English Medium</Link>
+                </td>
+              </tr>
+            )
+          })}
+        </tbody>
+      </table>
+
+      <p>
+        Here, you can download Matric{' '}
+        <strong>Biology 10th Class Book English Medium PDF </strong>
+        Format. This book is officially published by Punjab Text Book Board.
+      </p>
+      <PDFViewer pdfUrl={data.fileId_english} />
+      {MainDataClass10Book.map((data: any) => {
+        return (
+          <div key={data.title}>
+            <h2 className="">{data.title}</h2>
+            <div className="grid gap-4 md:grid-cols-2 grid-cols-1 my-5">
+              {data.child.map((data: any) => {
+                return (
+                  <CardSmall
+                    key={data.name}
+                    title={data.name}
+                    link={`/class-10/books/punjab-board-${data.slug}`}
+                  />
+                )
+              })}
+            </div>
+          </div>
+        )
+      })}
+    </Wrapper>
   )
+}
+
+const chapterDATA = [
+  { id: '1', link: '#' },
+  { id: '2', link: '#' },
+  { id: '3', link: '#' },
+  { id: '4', link: '#' },
+  { id: '5', link: '#' },
+  { id: '6', link: '#' },
+  { id: '7', link: '#' },
+  { id: '8', link: '#' },
+  { id: '9', link: '#' },
+]
+
+export const metadata: Metadata = {
+  title: data.title,
+  description: data.description,
+  keywords: data.keywords,
+  openGraph: {
+    title: data.title,
+    description: data.description,
+    url: data.url,
+    images: [
+      {
+        url: data.image,
+        alt: data.title,
+      },
+    ],
+  },
+  alternates: {
+    canonical: data.canonical,
+  },
+  robots: {
+    index: data.index,
+    follow: data.follow,
+    googleBot: {
+      index: data.index,
+      follow: data.follow,
+    },
+  },
+  twitter: {
+    title: data.title,
+    description: data.description,
+    images: {
+      url: data.image,
+      alt: data.title,
+    },
+  },
 }
--- src/app/(main)/(class)/class-12/books/punjab-board-biology/urdu-medium/page.tsx
+++ src/app/(main)/(class)/class-12/books/punjab-board-biology/urdu-medium/page.tsx
@@ -1,7 +1,85 @@
 import React from 'react'
+import { Metadata } from 'next'
+import Wrapper from '@/src/components/element/Wrapper'
+import PDFViewer from '@/src/components/element/PDFViewer'
 
-export default function page() {
+const data = {
+  title: 'Biology 10th Class Book Urdu Medium PDF Download | Punjab Board',
+  description:
+    'Download the 10th Class Biology book in Urdu Medium PDF, approved by the Punjab Board. Get the complete syllabus with detailed explanations and diagrams for effective exam preparation.',
+  canonical: '/class-10/books/punjab-board-biology/urdu-medium/',
+  index: true,
+  follow: true,
+  image: '/books/class-10/punjab/class-10-biology-book-punjab-board.webp',
+  url: 'https://growlearnhub.com/class-10/books/punjab-board-biology/urdu-medium/',
+  keywords: [
+    'biology class 10 punjab text book pdf',
+    'class 10 biology book pdf punjab board',
+    '10th class biology punjab text book',
+    'biology book class 10 punjab board',
+    'class 10 biology book punjab board',
+    'Biology 10th Class Book Urdu Medium PDF Download Punjab Board',
+  ],
+  fileId_urdu: '1ty3cPG8aWvjPla3xJAT4F__bLNJHKiZv',
+}
+
+export default function Page() {
   return (
-    <div>page</div>
+    <Wrapper
+      title={data.title}
+      url={data.canonical}
+      b1="12th Class"
+      b1Link="/class-12/"
+      b2="Books"
+      b2Link="/class-12/books/"
+      b3="Punjab Board Biology"
+      b3Link="/class-12/books/punjab-board-biology/"
+      b4="Biology 12th Class Book Urdu Medium"
+      image={data.image}
+    >
+      <h3>Biology 12th Class Book Urdu Medium PDF Download Punjab Board</h3>
+      <p>
+        Here, you can download Matric{' '}
+        <strong>Biology 12th Class Book Urdu Medium PDF</strong>
+        Format. This book is officially published by Punjab Text Book Board.
+      </p>
+      <PDFViewer pdfUrl={data.fileId_urdu} />
+    </Wrapper>
   )
+}
+
+export const metadata: Metadata = {
+  title: data.title,
+  description: data.description,
+  keywords: data.keywords,
+  openGraph: {
+    title: data.title,
+    description: data.description,
+    url: data.url,
+    images: [
+      {
+        url: 'https://nextjs.org/og.png',
+        alt: data.title,
+      },
+    ],
+  },
+  alternates: {
+    canonical: data.canonical,
+  },
+  robots: {
+    index: data.index,
+    follow: data.follow,
+    googleBot: {
+      index: data.index,
+      follow: data.follow,
+    },
+  },
+  twitter: {
+    title: data.title,
+    description: data.description,
+    images: {
+      url: 'https://nextjs.org/og.png',
+      alt: data.title,
+    },
+  },
 }
