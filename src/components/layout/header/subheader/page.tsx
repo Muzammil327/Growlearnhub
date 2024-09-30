9901395e65532461272fad68768d5a660f454042
Muzammil327 <mmuzammil327off@gmail.com>
2024-09-30 15:40:06 +0500

    updated: 3:40 9/30/2024

--- src/components/layout/header/subheader/page.tsx
+++ src/components/layout/header/subheader/page.tsx
@@ -1,14 +1,10 @@
 import Container from "@/src/components/element/container"
-import style from "./SubHeader.module.css"
 
 export default function SubHeader(props: { title: string }) {
   return (
     <section className="py-12 relative z-0 overflow-hidden bg-cover bg-no-repeat md:bg-fixed bg-center overlay">
       <Container>
-        <div className={`pt-8 ${style.hero}`}>
-          <div className={style.bg1} aria-hidden="true">
-            <div className={style.icon} />
-          </div>
+        <div className="pt-8 relative isolate">
           <div className="mx-auto py-8 sm:py-20 md:py-32">
             <div className="md:text-center text-left mt-4">
               <h1 className="tw">{props.title}</h1>
