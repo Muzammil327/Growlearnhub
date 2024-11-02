9c8bc6dca49d9180b51261e2b477230879f78c06
Muzammil327 <mmuzammil327off@gmail.com>
2024-11-02 21:26:10 +0500

    updated: version3

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,9 @@
 const nextConfig = {
   experimental: {
       serverComponentsExternalPackages: ['typeorm'],
+      appDir: true, // If you're using the app directory feature
     },
+    revalidate: 60,
 };
 
 export default nextConfig;
