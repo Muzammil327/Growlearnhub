62c3a36c12a3543a7a43760608555ec0474e79de
Muzammil327 <mmuzammil327off@gmail.com>
2024-11-03 09:48:48 +0500

    updated

---
+++ src/types/blogs.ts
@@ -0,0 +1,14 @@
+export interface PostCard {
+  tagslug: string
+  authorslug: string
+  id: string
+  date: string
+  title: string
+  slug: string
+  para: string
+  cat: string
+  author: string
+  tag: string
+  image: string
+  catslug: string
+}
---
+++ src/types/card.ts
@@ -0,0 +1,6 @@
+export interface SmallCardProps {
+  id?: number
+  title: string
+  link: string
+  className?: string
+}
