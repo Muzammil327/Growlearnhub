ce964d8ec9c0850fda5c35bcc0a6c43921520dbf
Muzammil327 <mmuzammil327off@gmail.com>
2024-11-26 17:44:54 +0500

    some error page updated

--- src/app/(main)/(class)/class-12/books/(punjab)/punjab-board-tarjuma-tul-quran/page.tsx
+++ src/app/(main)/(class)/class-12/books/(punjab)/punjab-board-tarjuma-tul-quran/page.tsx
@@ -1,87 +1,80 @@
-// import React from "react"
-// import { Metadata } from "next"
-// import PDFViewer from "@/src/components/elements/PDFViewer"
-// import Simplewrapper from "@/src/components/wrapper/Simplewrapper"
+import React from "react"
+import { Metadata } from "next"
+import PDFViewer from "@/src/components/elements/PDFViewer"
+import SimpleWrapper from "@/src/components/elements/SimpleWrapper"
 
-// const data = {
-//   title: "Tarjuma Tul Quran Class 12 Book PDF | 12th Class Punjab Text Book",
-//   description:
-//     "Download the official Tarjuma Tul Quran Class 12 textbook PDF by Punjab Board. This comprehensive guide covers the entire syllabus with clear explanation.",
-//   image: "/12th/book/punjab/class_12_tarjuma_tul_quran_book_punjab_board.webp",
-//   keywords: [
-//     "tarjuma-tul-quran class 12 punjab text book pdf",
-//     "class 12 tarjuma-tul-quran book pdf punjab board",
-//     "12th class tarjuma-tul-quran punjab text book",
-//     "tarjuma-tul-quran book class 12 punjab board",
-//     "class 12 tarjuma-tul-quran book punjab board"
-//   ],
-//   canonical: "/class-12/books/punjab-board-tarjuma-tul-quran/",
-//   url: "https://growlearnhub.com/class-12/books/punjab-board-tarjuma-tul-quran/",
-//   index: true,
-//   follow: true,
-//   fileId_urdu: "1qKP8ytj1ahOqFTE-y9oul_nFytNWtd6F",
-// }
+const data = {
+  title: "Tarjuma Tul Quran Class 12 Book PDF | 12th Class Punjab Text Book",
+  description:
+    "Download the official Tarjuma Tul Quran Class 12 textbook PDF by Punjab Board. This comprehensive guide covers the entire syllabus with clear explanation.",
+  image: "/12th/book/punjab/class_12_tarjuma_tul_quran_book_punjab_board.webp",
+  keywords: [
+    "tarjuma-tul-quran class 12 punjab text book pdf",
+    "class 12 tarjuma-tul-quran book pdf punjab board",
+    "12th class tarjuma-tul-quran punjab text book",
+    "tarjuma-tul-quran book class 12 punjab board",
+    "class 12 tarjuma-tul-quran book punjab board"
+  ],
+  canonical: "/class-12/books/punjab-board-tarjuma-tul-quran/",
+  url: "https://growlearnhub.com/class-12/books/punjab-board-tarjuma-tul-quran/",
+  index: true,
+  follow: true,
+  fileId_urdu: "1qKP8ytj1ahOqFTE-y9oul_nFytNWtd6F",
+}
 
-// export default function Page() {
-//   return (
-//     <Simplewrapper data={data}>
-//       <p>
-//         Are you looking for a{" "}
-//         <strong>Class 12 tarjuma tul quran book punjab board</strong>? Here, you
-//         can access all the books you need for free. Our website offers a
-//         complete collection of books for students in Class 9, 10, 11, and 12.
-//         Whether you&apos;re preparing for exams or need extra resources,
-//         we&apos;ve got you covered.
-//       </p>
-//       <h3>Download Class 12 Tarjuma Tul Quran Book Punjab Board</h3>
-//       <p>
-//         Here, you can download Matric class 12 Tarjuma Tul Quran Text Book in PDF
-//         Format. This book is officially published by Punjab Text Book Board.
-//       </p>
-//       <PDFViewer pdfUrl={data.fileId_urdu} />
-//     </Simplewrapper>
-//   )
-// }
-
-// export const metadata: Metadata = {
-//   title: data.title,
-//   description: data.description,
-//   keywords: data.keywords,
-//   openGraph: {
-//     title: data.title,
-//     description: data.description,
-//     url: data.url,
-//     images: [
-//       {
-//         url: data.image,
-//         alt: data.title
-//       }
-//     ]
-//   },
-//   alternates: {
-//     canonical: data.canonical
-//   },
-//   robots: {
-//     index: data.index,
-//     follow: data.follow,
-//     googleBot: {
-//       index: data.index,
-//       follow: data.follow
-//     }
-//   },
-//   twitter: {
-//     title: data.title,
-//     description: data.description,
-//     images: {
-//       url: data.image,
-//       alt: data.title
-//     }
-//   }
-// }
-import React from 'react'
-
-export default function page() {
+export default function Page() {
   return (
-    <div>page</div>
+    <SimpleWrapper data={data}>
+      <p>
+        Are you looking for a{" "}
+        <strong>Class 12 tarjuma tul quran book punjab board</strong>? Here, you
+        can access all the books you need for free. Our website offers a
+        complete collection of books for students in Class 9, 10, 11, and 12.
+        Whether you&apos;re preparing for exams or need extra resources,
+        we&apos;ve got you covered.
+      </p>
+      <h3>Download Class 12 Tarjuma Tul Quran Book Punjab Board</h3>
+      <p>
+        Here, you can download Matric class 12 Tarjuma Tul Quran Text Book in PDF
+        Format. This book is officially published by Punjab Text Book Board.
+      </p>
+      <PDFViewer pdfUrl={data.fileId_urdu} />
+    </SimpleWrapper>
   )
+}
+
+export const metadata: Metadata = {
+  title: data.title,
+  description: data.description,
+  keywords: data.keywords,
+  openGraph: {
+    title: data.title,
+    description: data.description,
+    url: data.url,
+    images: [
+      {
+        url: data.image,
+        alt: data.title
+      }
+    ]
+  },
+  alternates: {
+    canonical: data.canonical
+  },
+  robots: {
+    index: data.index,
+    follow: data.follow,
+    googleBot: {
+      index: data.index,
+      follow: data.follow
+    }
+  },
+  twitter: {
+    title: data.title,
+    description: data.description,
+    images: {
+      url: data.image,
+      alt: data.title
+    }
+  }
 }
