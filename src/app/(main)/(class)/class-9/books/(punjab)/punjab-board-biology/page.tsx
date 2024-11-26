bf81e7f8224b64de32725147738506909aaf9673
Muzammil327 <mmuzammil327off@gmail.com>
2024-11-26 18:24:44 +0500

    some error page updated

--- src/app/(main)/(class)/class-9/books/(punjab)/punjab-board-biology/page.tsx
+++ src/app/(main)/(class)/class-9/books/(punjab)/punjab-board-biology/page.tsx
@@ -1,118 +1,111 @@
-// import React from "react"
-// import { Metadata } from "next"
-// import CardSmall from "@/src/components/card/cardSmall/cardSmall"
-// import Simplewrapper from "@/src/components/wrapper/Simplewrapper"
-// import PDFViewer from "@/src/components/elements/PDFViewer"
+import React from "react"
+import { Metadata } from "next"
+import CardSmall from "@/src/components/card/cardSmall/cardSmall"
+import PDFViewer from "@/src/components/elements/PDFViewer"
+import SimpleWrapper from "@/src/components/elements/SimpleWrapper"
 
-// const data = {
-//   title: "Biology Class 9 Book PDF | 9th Class Punjab Text Book",
-//   description:
-//     "Download the official Biology Class 9 textbook PDF by Punjab Board. This comprehensive guide covers the entire syllabus with clear explanation.",
-//   keywords: [
-//     "biology class 9 punjab text book pdf",
-//     "class 9 biology book pdf punjab board",
-//     "9th class biology punjab text book",
-//     "biology book class 9 punjab board",
-//     "class 9 biology book punjab board"
-//   ],
-//   image: "/9th/book/punjab/class_9_biology_book_punjab_board.webp",
-//   canonical: "/class-9/books/punjab-board-biology/",
-//   url: "https://growlearnhub.com/class-9/books/punjab-board-biology/",
-//   index: true,
-//   follow: true,
-//   fileId_english: "1MFRZ5yO0SrrfTkAM_ewXBp_Pec4zTOA-",
-//   fileId_urdu: "1ty3cPG8aWvjPla3xJAT4F__bLNJHKiZv",
-// }
+const data = {
+  title: "Biology Class 9 Book PDF | 9th Class Punjab Text Book",
+  description:
+    "Download the official Biology Class 9 textbook PDF by Punjab Board. This comprehensive guide covers the entire syllabus with clear explanation.",
+  keywords: [
+    "biology class 9 punjab text book pdf",
+    "class 9 biology book pdf punjab board",
+    "9th class biology punjab text book",
+    "biology book class 9 punjab board",
+    "class 9 biology book punjab board"
+  ],
+  image: "/9th/book/punjab/class_9_biology_book_punjab_board.webp",
+  canonical: "/class-9/books/punjab-board-biology/",
+  url: "https://growlearnhub.com/class-9/books/punjab-board-biology/",
+  index: true,
+  follow: true,
+  fileId_english: "1MFRZ5yO0SrrfTkAM_ewXBp_Pec4zTOA-",
+  fileId_urdu: "1ty3cPG8aWvjPla3xJAT4F__bLNJHKiZv",
+}
 
-// export default function Page() {
-//   return (
-//     <Simplewrapper data={data}>
-//       <div className="grid gap-4 grid-cols-1 my-5">
-//         {mainData.map((data: any) => {
-//           return (
-//             <CardSmall
-//               key={data.name}
-//               title={data.name}
-//               link={`class-9/books/punjab-board-biology/${data.slug}-medium`}
-//             />
-//           )
-//         })}
-//       </div>
-//       <p>
-//         Are you looking for a <strong>Class 9 biology book punjab board</strong>
-//         ? Here, you can access all the books you need for free. Our website
-//         offers a complete collection of books for students in Class 9, 10, 11,
-//         and 12. Whether you&apos;re preparing for exams or need extra resources,
-//         we&apos;ve got you covered.
-//       </p>
-//       <h3>Download Class 9 Biology Book English Medium Punjab Board</h3>
-//       <p>
-//         Here, you can download Matric class 9 Biology Text Book in PDF Format
-//         for English Medium. This book is officially published by Punjab Text
-//         Book Board.
-//       </p>
-//       <PDFViewer pdfUrl={data.fileId_english} />
-//       <h3>Download 9th Class Biology Textbook Urdu Medium Punjab Board</h3>
-//       <p>
-//         Here, you can download Matric class 9 Biology Text Book in PDF Format
-//         for Urdu Medium. This book is officially published by Punjab Text Book
-//         Board.
-//       </p>
-//       <PDFViewer pdfUrl={data.fileId_urdu} />
-//     </Simplewrapper>
-//   )
-// }
-
-// const mainData = [
-//   {
-//     name: "Class 9 Biology Book PDF Punjab Board English Medium",
-//     slug: "english"
-//   },
-//   {
-//     name: "Biology Class 9 Punjab Board Text Book PDF Urdu Medium",
-//     slug: "urdu"
-//   }
-// ]
-
-// export const metadata: Metadata = {
-//   title: data.title,
-//   description: data.description,
-//   keywords: data.keywords,
-//   openGraph: {
-//     title: data.title,
-//     description: data.description,
-//     url: data.url,
-//     images: [
-//       {
-//         url: data.image,
-//         alt: data.title
-//       }
-//     ]
-//   },
-//   alternates: {
-//     canonical: data.canonical
-//   },
-//   robots: {
-//     index: data.index,
-//     follow: data.follow,
-//     googleBot: {
-//       index: data.index,
-//       follow: data.follow
-//     }
-//   },
-//   twitter: {
-//     title: data.title,
-//     description: data.description,
-//     images: {
-//       url: data.image,
-//       alt: data.title
-//     }
-//   }
-// }
-import React from 'react'
-
-export default function page() {
+export default function Page() {
   return (
-    <div>page</div>
+    <SimpleWrapper data={data}>
+      <div className="grid gap-4 grid-cols-1 my-5">
+        {mainData.map((data: any) => {
+          return (
+            <CardSmall
+              key={data.name}
+              title={data.name}
+              link={`class-9/books/punjab-board-biology/${data.slug}-medium`}
+            />
+          )
+        })}
+      </div>
+      <p>
+        Are you looking for a <strong>Class 9 biology book punjab board</strong>
+        ? Here, you can access all the books you need for free. Our website
+        offers a complete collection of books for students in Class 9, 10, 11,
+        and 12. Whether you&apos;re preparing for exams or need extra resources,
+        we&apos;ve got you covered.
+      </p>
+      <h3>Download Class 9 Biology Book English Medium Punjab Board</h3>
+      <p>
+        Here, you can download Matric class 9 Biology Text Book in PDF Format
+        for English Medium. This book is officially published by Punjab Text
+        Book Board.
+      </p>
+      <PDFViewer pdfUrl={data.fileId_english} />
+      <h3>Download 9th Class Biology Textbook Urdu Medium Punjab Board</h3>
+      <p>
+        Here, you can download Matric class 9 Biology Text Book in PDF Format
+        for Urdu Medium. This book is officially published by Punjab Text Book
+        Board.
+      </p>
+      <PDFViewer pdfUrl={data.fileId_urdu} />
+    </SimpleWrapper>
   )
+}
+
+const mainData = [
+  {
+    name: "Class 9 Biology Book PDF Punjab Board English Medium",
+    slug: "english"
+  },
+  {
+    name: "Biology Class 9 Punjab Board Text Book PDF Urdu Medium",
+    slug: "urdu"
+  }
+]
+
+export const metadata: Metadata = {
+  title: data.title,
+  description: data.description,
+  keywords: data.keywords,
+  openGraph: {
+    title: data.title,
+    description: data.description,
+    url: data.url,
+    images: [
+      {
+        url: data.image,
+        alt: data.title
+      }
+    ]
+  },
+  alternates: {
+    canonical: data.canonical
+  },
+  robots: {
+    index: data.index,
+    follow: data.follow,
+    googleBot: {
+      index: data.index,
+      follow: data.follow
+    }
+  },
+  twitter: {
+    title: data.title,
+    description: data.description,
+    images: {
+      url: data.image,
+      alt: data.title
+    }
+  }
 }
