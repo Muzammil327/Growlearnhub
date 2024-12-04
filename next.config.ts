398707949aae4e33440df394f6328a5551c11b6e
Muzammil327 <mmuzammil327off@gmail.com>
2024-12-04 11:10:33 +0500

    updated: latest

--- next.config.ts
+++ next.config.ts
@@ -5,24 +5,25 @@ const nextConfig = {
   images: {
     domains: ["rainbowthemes.net"],
   },
-  // async redirects() {
-  //   return [
-  //     {
-  //       source: "/:path*",
-  //       has: [{ type: "host", value: "www.growlearnhub.com" }],
-  //       destination: "https://growlearnhub.com/:path*",
-  //       permanent: true,
-  //     },
-  //   ];
-  // },
-  // trailingSlash: (path: string): boolean => {
-  //   if (path.startsWith('/api')) {
-  //     return false;
-  //   }
-  //   return true;
-  // },
-  // reactStrictMode: true,
-  // swcMinify: true,
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        has: [{ type: "host", value: "www.growlearnhub.com" }],
+        destination: "https://growlearnhub.com/:path*",
+        permanent: true,
+      },
+    ];
+  },
+  trailingSlash: (path: string): boolean => {
+    console.log('trailingSlash:', path);
+    if (path.startsWith('/api/')) {
+      return false;
+    }
+    return true;
+  },
+  reactStrictMode: true,
+  swcMinify: true,
 };
 
 export default nextConfig;
