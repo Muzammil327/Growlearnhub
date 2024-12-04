8a7c3b2d8cfd388812b509bf176cb1b7160c4911
Muzammil327 <mmuzammil327off@gmail.com>
2024-12-04 13:09:44 +0500

    latest update

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,12 @@ const nextConfig = {
   async redirects() {
     return [
       {
+        source: "/:path*",
+        has: [{ type: "host", value: "www.growlearnhub.com" }],
+        destination: "https://growlearnhub.com/:path*/",  // Ensure trailing slash in the destination URL
+        permanent: true,
+      },
+      {
         source: "/sitemap.xml/",
         destination: "/sitemap.xml",
         permanent: true,
