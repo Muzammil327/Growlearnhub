959237cd074b6bc1c1f5a088a9cac13b2095fc7b
Muzammil327 <mmuzammil327off@gmail.com>
2024-12-05 22:19:32 +0500

    update dynamic data

--- src/lib/readJsonFile.ts
+++ src/lib/readJsonFile.ts
@@ -2,16 +2,27 @@ import path from "path";
 import fs from "fs";
 
 const readJsonFile = (fileName: string) => {
-  const filePath = path.join(process.cwd(), "src", "data", "mcqs", fileName); // Adjust the path to your file location
-  const fileContent = fs.readFileSync(filePath, "utf-8");
-  return JSON.parse(fileContent);
+  const filePath = path.join(process.cwd(), "src", "data", "mcqs", fileName);
+  try {
+    const fileContent = fs.readFileSync(filePath, "utf-8");
+    return JSON.parse(fileContent);
+  } catch (error) {
+    console.error(`Error reading file: ${error}`);
+    return [];
+  }
 };
 
-// Dynamically load data based on slug (you can optimize to load only data1.json or data2.json based on some logic)
-const data1 = readJsonFile("data1.json");
-const data2 = readJsonFile("data2.json");
-const data3 = readJsonFile("data3.json");
-const data4 = readJsonFile("data4.json");
+const filesToLoad = [
+  "data1.json", 
+  "data2.json", 
+  "data3.json", 
+  "data4.json",
+  "data5.json"
+  // "data6.json", 
+  // "data7.json", 
+  // "data8.json"
+];
 
-// Combine data only when needed
-export const combinedData = [...data1, ...data2, ...data3, ...data4];
+const combinedData = filesToLoad.flatMap(readJsonFile);
+
+export { combinedData };
