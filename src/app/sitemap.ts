bc6b42e03e446401d277de7e8f8356af225225ca
Muzammil327 <mmuzammil327off@gmail.com>
2024-12-06 22:42:47 +0500

    updated

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -18,17 +18,17 @@ const FRONTEND_URL = process.env.NEXT_PUBLIC_SITE_URL;
 
 export default async function sitemap(): Promise<SitemapFile[]> {
   // const response = await axios.get(`${FRONTEND_URL}/api/sitemaps`);
-  const response = await axios.get(`https://www.growlearnhub.com/api/sitemaps`);
+  // const response = await axios.get(`https://www.growlearnhub.com/api/sitemaps`);
 
-  const McqsSlug = response.data;
+  // const McqsSlug = response.data;
 
   try {
-    const RoutesQuestionData: SitemapFile[] = McqsSlug.map((data: string) => ({
-      url: `${FRONTEND_URL}/mcqs-point/${data}/`,
-      lastModified: new Date().toISOString(),
-      priority: 0.8,
-      changeFrequency: "weekly" as "weekly",
-    }));
+    // const RoutesQuestionData: SitemapFile[] = McqsSlug.map((data: string) => ({
+    //   url: `${FRONTEND_URL}/mcqs-point/${data}/`,
+    //   lastModified: new Date().toISOString(),
+    //   priority: 0.8,
+    //   changeFrequency: "weekly" as "weekly",
+    // }));
 
     const RoutesData: SitemapFile[] = data.map((data) => ({
       url: `${FRONTEND_URL}/${data}/`,
@@ -46,7 +46,7 @@ export default async function sitemap(): Promise<SitemapFile[]> {
         changeFrequency: "always",
       },
       ...RoutesData,
-      ...RoutesQuestionData,
+      // ...RoutesQuestionData,
     ];
 
     return routes;
